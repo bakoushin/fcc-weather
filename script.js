059e94523340eca5ab95285414b5b45ef3f95475
Alex Bakoushin <alex@bakoush.in>
2017-12-11 21:41:37 +0300

    Stylistic refactoring

--- script.js
+++ script.js
@@ -92,13 +92,13 @@ fahrenheitButton.click(function (event) {
 });
 
 if (!navigator.geolocation) {
-  showError(`Oops! Your browser doesn't support geolocation.`);
+  showError("Oops! Your browser doesn't support geolocation.");
 } else {
   navigator.geolocation.getCurrentPosition(function (location) {
     var latitude = location.coords.latitude;
     var longitude = location.coords.longitude;
     var weatherData; // for sharing weather data between promises
-    var weatherRequest = $.ajax(`https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`)
+    var weatherRequest = $.ajax('https://fcc-weather-api.glitch.me/api/current?lat=' + latitude + '&lon=' + longitude)
     .then(function (res) {
       weatherData = res;
       var tempCurrent = weatherData.main.temp;
@@ -125,7 +125,9 @@ if (!navigator.geolocation) {
       
       weatherTextView.text(weatherText);
       if (weatherIconSrc) {
-        weatherIconView.append($(`<img src="${weatherIconSrc}" class="weather-description__icon">`));
+        $('<img>').attr('src', weatherIconSrc)
+          .addClass('weather-description__icon')
+          .appendTo(weatherIconView);
       }
 
       humidityView.text(humidity);
@@ -140,7 +142,7 @@ if (!navigator.geolocation) {
 
     weatherRequest.then(function () {
       var countryAlphaCode = weatherData.sys.country;
-      return $.ajax(`https://restcountries.eu/rest/v2/alpha/${countryAlphaCode}`);
+      return $.ajax('https://restcountries.eu/rest/v2/alpha/' + countryAlphaCode);
     })
     .then(function (res) {
       if (res && res.name) {
@@ -157,7 +159,7 @@ if (!navigator.geolocation) {
     var setLocationView = function(country) {
       var location = weatherData.name;
       var countryName = country || weatherData.sys.country;
-      locationView.text(`${location}, ${countryName}`);
+      locationView.text(location +', ' + countryName);
     }
 
     weatherRequest.then(function () {
@@ -166,57 +168,54 @@ if (!navigator.geolocation) {
       var isDaytime = isDaytimeNow(sunrise, sunset);
       var weatherDescription = weatherData.weather[0].description;
       var imageQuery = weatherDescription + ' ' + (isDaytime ? 'day' : 'night');
-      return $.ajax(`https://berry-drill.glitch.me/${imageQuery}`);
+      return $.ajax('https://berry-drill.glitch.me/' + imageQuery);
     })
     .then(function (res) {
       // preload image before updating background
       var imageSrc = res.urls.regular;
-      $(`<img src="${imageSrc}">`).on('load', function () {
+      $('<img>').attr('src', imageSrc).on('load', function () {
         $(this).remove;
         containerView
-          .css('background-image', `url(${imageSrc})`)
+          .css('background-image', 'url(' + imageSrc + ')')
           .removeClass('container--faded');
         // author credits
         var authorName = res.user.name;
         var authorProfile = res.user.links.html;
         var unsplashUTM = '?utm_source=freeCodeCamp-local-weather-bakoushin&utm_medium=referral&utm_campaign=api-credit';
-        var author = `Photo by <a class="author__link" href="${authorProfile + unsplashUTM}" target="_blank">${authorName}</a> /
-        <a class="author__link" href="https://unsplash.com${unsplashUTM}" target="_blank">Unsplash</a>`;
-        authorView.html(author);
-        authorView.addClass('author--visible');
+        var author = 'Photo by <a class="author__link" href="' + authorProfile + unsplashUTM + '" target="_blank">' + authorName + '</a> / <a class="author__link" href="https://unsplash.com' + unsplashUTM + '" target="_blank">Unsplash</a>';
+        authorView.html(author).addClass('author--visible');
       });
     })
     .catch(function (error) {
       console.log(error);
     });
   }, function(err) {
-    showError(`Oops! ${err.message} (error code: ${err.code})`);
+    showError('Oops! ' + err.message + ' (error code: ' + err.code + ')');
   });
 }
 
-var showError = function(text) {
-  errorView.text(text);
-  errorView.addClass('error--visible');
+function showError (text) {
+  errorView.text(text).addClass('error--visible');
   hideLoader();
 }
 
-var hideLoader = function() {
+function hideLoader () {
   loaderView.addClass('loader--hidden');
   loaderContentView.addClass('loader__content--hidden');
 }
 
 // convert temperature form Celsius to Fahrenheit degrees
-var celsiusToFahrenheit = function (temp) {
+function celsiusToFahrenheit (temp) {
   return Math.round(temp * 9 / 5 + 32);
 }
 
 // convert pressure from dPa to mmHg
-var dPaToMmHg = function (pressure) {
+function dPaToMmHg (pressure) {
   return Math.round(pressure / 10 * 760.001 / 101.325);
 }
 
 // return true if now is daytime based on sunrise and sunset provided
-var isDaytimeNow = function (sunrise, sunset) {
+function isDaytimeNow (sunrise, sunset) {
   var now = Date.now();
   return (now >= sunrise && now < sunset);
 }
@@ -224,8 +223,7 @@ var isDaytimeNow = function (sunrise, sunset) {
 // animate incrementing/decrementing number
 $.fn.extend({
   animateValueChange: function(value) {
-    return this
-      .prop('_counter', this.text())
+    return this.prop('_counter', this.text())
       .animate({
         _counter: value
       }, {
